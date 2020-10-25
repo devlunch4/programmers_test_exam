19f174cf3edec1ad874c06d53d6d8ce99df9431b
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-26 01:45:14 +0900

    Update [String-Date]_오랜 _기간_보호한_동물(2).sql

--- SQL_LV3/[String-Date]_오랜 _기간_보호한_동물(2).sql
+++ SQL_LV3/[String-Date]_오랜 _기간_보호한_동물(2).sql
@@ -1,2 +1,17 @@
 --코딩테스트 연습>String, Date>오랜 기간 보호한 동물(2)
 
+-- MySQL 코드를 입력하세요
+SELECT o.animal_id, o.name
+FROM animal_ins i JOIN animal_outs o
+ON i.animal_id = o.animal_id
+ORDER BY (o.datetime - i.datetime) DESC
+LIMIT 2;
+
+-- Oracle 코드를 입력하세요
+SELECT *
+FROM (SELECT o.animal_id, o.name
+    FROM animal_ins i JOIN animal_outs o
+    ON i.animal_id = o.animal_id
+    ORDER BY (o.datetime - i.datetime) DESC
+     )
+WHERE ROWNUM < 3;
