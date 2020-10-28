c5fc0e0d0084583535e6f5279ac12799cf2847e8
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-28 10:11:07 +0900

    gist prgrms DONE

--- SQL_LV1/[IS NULL]_이름이_있는_동물의_아이디.sql
+++ SQL_LV1/[IS NULL]_이름이_있는_동물의_아이디.sql
@@ -11,3 +11,6 @@ SELECT animal_id
 FROM animal_ins 
 WHERE name IS NOT NULL
 ORDER BY animal_id ASC;
+
+https://programmers.co.kr/learn/courses/30/lessons/59407
+https://gist.github.com/devlunch4/4b4439eaa0c4ea96e6d2a8329d168cf8
