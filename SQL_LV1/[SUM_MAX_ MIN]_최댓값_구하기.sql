dbdbe25c56c1ee352239478aefdbd67b44795554
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-28 10:14:45 +0900

    gist prgrms DONE

--- SQL_LV1/[SUM_MAX_ MIN]_최댓값_구하기.sql
+++ SQL_LV1/[SUM_MAX_ MIN]_최댓값_구하기.sql
@@ -5,3 +5,6 @@ SELECT MAX(datetime) FROM Animal_ins;
 
 -- Oracle 코드를 입력하세요 
 SELECT MAX(datetime) FROM Animal_ins;
+
+https://programmers.co.kr/learn/courses/30/lessons/59415
+https://gist.github.com/devlunch4/d739938f574dc4747a81276ade9f74ed
