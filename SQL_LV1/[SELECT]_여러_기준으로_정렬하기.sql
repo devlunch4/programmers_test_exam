5e344cb8efada2e2e13ca8194a07ac9410709d26
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-28 20:11:09 +0900

    gist blog DONE

--- SQL_LV1/[SELECT]_여러_기준으로_정렬하기.sql
+++ SQL_LV1/[SELECT]_여러_기준으로_정렬하기.sql
@@ -9,3 +9,6 @@ ORDER BY name ASC, datetime DESC;
 SELECT animal_id, name, datetime
 FROM animal_ins
 ORDER BY name ASC, datetime DESC;
+
+https://gist.github.com/devlunch4/14eb1dc731f9e2dc117ae50f6bb3f3e0
+https://programmers.co.kr/learn/courses/30/lessons/59404
