27ad4c05b226cc48078f6da2295e92f9c3ea150e
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-28 18:27:18 +0900

    gist blogDONE

--- SQL_LV1/[SELECT]_아픈_동물_찾기.sql
+++ SQL_LV1/[SELECT]_아픈_동물_찾기.sql
@@ -11,3 +11,6 @@ SELECT animal_id, name
 FROM animal_ins
 WHERE intake_condition = 'Sick'
 ORDER BY animal_id;
+
+https://gist.github.com/devlunch4/2b5abbed12588718733ca2c1c390ddb4
+https://programmers.co.kr/learn/courses/30/lessons/59036
