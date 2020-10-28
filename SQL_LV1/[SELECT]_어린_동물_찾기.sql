4223c80f6ccd38c7c26fedf15520d667964de641
devlunch4 <70561423+devlunch4@users.noreply.github.com>
2020-10-28 20:06:27 +0900

    gist BLOG DONE

--- SQL_LV1/[SELECT]_어린_동물_찾기.sql
+++ SQL_LV1/[SELECT]_어린_동물_찾기.sql
@@ -11,3 +11,7 @@ SELECT animal_id, name
 FROM animal_ins 
 WHERE intake_condition 
 NOT LIKE 'Aged' ORDER BY animal_id
+
+
+https://programmers.co.kr/learn/courses/30/lessons/59037
+https://gist.github.com/devlunch4/aaca78692be2d5f68194cf851b3fd165
